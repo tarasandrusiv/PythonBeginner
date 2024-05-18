c67659aa0802a2a2e3217a020892c4ba28d0f3a4
tarasandrusiv <tarasandrusivkb24@gmail.com>
2024-05-18 13:35:23 +0200

    contact (task 14)

---
+++ homework_2/contact/contact.py
@@ -0,0 +1,7 @@
+my_name = input("My name is: ")
+my_age = input("My age is: ")
+my_address = input("My address is: ")
+my_phone = input("My phone is: ")
+
+my_dict = {'name': my_name, 'age': my_age, 'address': my_address, 'phone': my_phone}
+print("Contact created: ", my_dict)
